84a3bc16c42d654643d405a6f2a05c579e52f6b5
Matthew Smith <msmith2@udtonline.com>
2024-01-10 14:44:43 -0500

    Changed output to follow best practice and only return the generated password

--- Invoke-PasswordGen.psm1
+++ Invoke-PasswordGen.psm1
@@ -15,25 +15,16 @@ None
 String
 .EXAMPLE
 Invoke-PasswordGen
-A password has been generated for you:
 
 soap CHAIN constant 7
-
-This password has been copied to the clipboard.
 .EXAMPLE
 Invoke-PasswordGen -NoSpaces
-A password has been generated for you:
 
 LOAT-penny-FLU-4
-
-This password has been copied to the clipboard.
 .EXAMPLE
 Invoke-PasswordGen -Secure
-A SECURE password has been generated:
 
 rumor1@solo7=choke5!pupil9-MARSH4#SLAP2$
-
-This password has been copied to the clipboard.
 .NOTES
 Author: msmith719
 
@@ -41,12 +32,11 @@ Possible Improvements:
 1) Cache the json file instead of downloading it each time.
 2) Create a -Local parameter that uses the cached json file if available and uses the $wordList  array if the json does not exist. Could also support an imput for a word list.
 #>
-
 function Invoke-PasswordGen {
     param (
-        [Parameter(Mandatory=$false)] [Switch]$NoSpaces,
-        [Parameter(Mandatory=$false)] [Switch]$Secure,
-        [Parameter(Mandatory=$false)] [Switch]$TestLocal
+        [Parameter(Mandatory = $false)] [Switch]$NoSpaces,
+        [Parameter(Mandatory = $false)] [Switch]$Secure,
+        [Parameter(Mandatory = $false)] [Switch]$TestLocal
     )
 
     $url = "https://randomwordgenerator.com/json/words_ws.json"
@@ -60,7 +50,8 @@ function Invoke-PasswordGen {
     # We then get the HTTP code as an integer.
     if ( !$TestLocal) {
         $HTTP_Status = [int]$HTTP_Response.StatusCode
-    } else {
+    }
+    else {
         $HTTP_Status = 404
         Write-Host "HELLO! This is an EXAMPLE of an error that would be shown if the JSON file could not be reached." -ForegroundColor Magenta
         Write-Host @"
@@ -74,32 +65,32 @@ At line:13 char:5                                                               
         $json = (New-Object System.Net.WebClient).DownloadString($url)
         $data = $json | ConvertFrom-Json
         
-        $words = Get-Random -InputObject ($data.data.word | ?{$_.numSyllables -lt 3}).value -Count 10
+        $words = Get-Random -InputObject ($data.data.word | ? { $_.numSyllables -lt 3 }).value -Count 10
     }
     Else {
-        $wordList = @("bless","aloof","fight","scene","movie","month","entry","bland","leave","story","spite","study","flush","agony","reign",`
-            "dance","bible","theft","cheat","table","refer","begin","style","deter","harsh","yearn","shave","sting","shark","still","right",`
-            "blade","fence","dairy","groan","brush","issue","other","quota","pluck","prove","plane","ghost","shaft","learn","greet","value",`
-            "trait","mercy","plant","tired","crash","level","peace","glove","owner","orbit","water","toast","cater","salon","grand","rumor",`
-            "truck","berry","guilt","fibre","beard","decay","front","layer","flock","pilot","smash","snarl","force","class","glass","grant",`
-            "watch","knock","start","brown","shout","guess","brick","cable","organ","loose","snail","store","fresh","frame","giant","stool",`
-            "drain","tiger","heavy","favor","trunk","waste","blind","color","court","reach","judge","faith","print","blast","linen","wreck",`
-            "stick","tract","knife","young","green","quiet","eject","prize","bunny","throw","thick","stamp","party","onion","sweat","drink",`
-            "storm","dough","arena","drive","chalk","slump","block","rally","train","sword","power","wheel","voter","title","steak","miner",`
-            "marsh","great","spare","write","salad","union","angle","shock","rabbit","split","arrow","draft","sharp","scale","share","carve",`
-            "truth","elect","cross","chord","smile","scrap","angel","feast","slide","spine","score","burst","doubt","carry","blame","forge",`
-            "think","spoil","wound","light","shelf","grind","embox","proof","kneel","screw","tense","build","honor","fault","guide","virus",`
-            "stand","piece","worry","climb","weave","jewel","thigh","offer","tight","dream","proud","sniff","outer","irony","tasty","blank",`
-            "flesh","motif","radio","clerk","crowd","ridge","turtle")
-            Write-Host "*******************" -ForegroundColor Magenta
-            Write-Host "*******************" -ForegroundColor Magenta
-            Write-Host ""
-            Write-Host "The normal word list JSON file could not be reached. This password will be created Using the backup word list." -ForegroundColor Magenta
-            write-host  "FYI, this word list is limited to" $wordList.Count "words." -ForegroundColor Magenta
-            Write-Host ""
-            Write-Host "*******************" -ForegroundColor Magenta
-            Write-Host "*******************" -ForegroundColor Magenta
-            $words = Get-Random -InputObject $wordList -Count 10
+        $wordList = @("bless", "aloof", "fight", "scene", "movie", "month", "entry", "bland", "leave", "story", "spite", "study", "flush", "agony", "reign", `
+                "dance", "bible", "theft", "cheat", "table", "refer", "begin", "style", "deter", "harsh", "yearn", "shave", "sting", "shark", "still", "right", `
+                "blade", "fence", "dairy", "groan", "brush", "issue", "other", "quota", "pluck", "prove", "plane", "ghost", "shaft", "learn", "greet", "value", `
+                "trait", "mercy", "plant", "tired", "crash", "level", "peace", "glove", "owner", "orbit", "water", "toast", "cater", "salon", "grand", "rumor", `
+                "truck", "berry", "guilt", "fibre", "beard", "decay", "front", "layer", "flock", "pilot", "smash", "snarl", "force", "class", "glass", "grant", `
+                "watch", "knock", "start", "brown", "shout", "guess", "brick", "cable", "organ", "loose", "snail", "store", "fresh", "frame", "giant", "stool", `
+                "drain", "tiger", "heavy", "favor", "trunk", "waste", "blind", "color", "court", "reach", "judge", "faith", "print", "blast", "linen", "wreck", `
+                "stick", "tract", "knife", "young", "green", "quiet", "eject", "prize", "bunny", "throw", "thick", "stamp", "party", "onion", "sweat", "drink", `
+                "storm", "dough", "arena", "drive", "chalk", "slump", "block", "rally", "train", "sword", "power", "wheel", "voter", "title", "steak", "miner", `
+                "marsh", "great", "spare", "write", "salad", "union", "angle", "shock", "rabbit", "split", "arrow", "draft", "sharp", "scale", "share", "carve", `
+                "truth", "elect", "cross", "chord", "smile", "scrap", "angel", "feast", "slide", "spine", "score", "burst", "doubt", "carry", "blame", "forge", `
+                "think", "spoil", "wound", "light", "shelf", "grind", "embox", "proof", "kneel", "screw", "tense", "build", "honor", "fault", "guide", "virus", `
+                "stand", "piece", "worry", "climb", "weave", "jewel", "thigh", "offer", "tight", "dream", "proud", "sniff", "outer", "irony", "tasty", "blank", `
+                "flesh", "motif", "radio", "clerk", "crowd", "ridge", "turtle")
+        Write-Host "*******************" -ForegroundColor Magenta
+        Write-Host "*******************" -ForegroundColor Magenta
+        Write-Host ""
+        Write-Host "The normal word list JSON file could not be reached. This password will be created Using the backup word list." -ForegroundColor Magenta
+        write-host "FYI, this word list is limited to" $wordList.Count "words." -ForegroundColor Magenta
+        Write-Host ""
+        Write-Host "*******************" -ForegroundColor Magenta
+        Write-Host "*******************" -ForegroundColor Magenta
+        $words = Get-Random -InputObject $wordList -Count 10
     }
 
     # Finally, we clean up the http request by closing it.
@@ -119,7 +110,8 @@ At line:13 char:5                                                               
                 $words[0] + " " + $words[1].ToUpper() + " " + $words[2] + " " + (Get-Random -InputObject $numbers)
             )
             $password = Get-Random -InputObject $options
-        } else {
+        }
+        else {
             $sep = (Get-Random -InputObject $seperators)
             $options = @(
                 $words[0].ToUpper() + $sep + $words[1] + $sep + $words[2].ToUpper() + $sep + (Get-Random -InputObject $numbers)
@@ -127,7 +119,8 @@ At line:13 char:5                                                               
             )
             $password = Get-Random -InputObject $options
         }
-    } else {
+    }
+    else {
         $sep = (Get-Random -InputObject $seperators)
         $specChar = (Get-Random -InputObject $specialChars -Count 10)
         $num = (Get-Random -InputObject $numbers -Count 10)
@@ -135,34 +128,14 @@ At line:13 char:5                                                               
         
         $password = ""
         
-        for ($i=0; $i -lt 6; $i++) {
-            $password = [System.String]::Concat($password,(Get-Random $words[$i].ToUpper(),$words[$i].ToLower()),$num[$i] + $specChar[$i])
+        for ($i = 0; $i -lt 6; $i++) {
+            $password = [System.String]::Concat($password, (Get-Random $words[$i].ToUpper(), $words[$i].ToLower()), $num[$i] + $specChar[$i])
         }
     }
 
-    $password | clip
-
-    Write-Host @'
-
-'@
-
-    Write-Host @"
-    
-     .--.
-    /.-. '----------.
-    \'-' .--"--""-"-'
-     '--'
-
-"@ -ForegroundColor Cyan
-    if ($Secure) {
-        Write-Host "A " -ForegroundColor Green -NoNewline
-        Write-Host "SECURE " -ForegroundColor DarkRed -NoNewline
-        Write-Host "password has been generated:" -ForegroundColor Green
-    } else {
-        Write-Host "A password has been generated for you:" -ForegroundColor Green
+    if (Get-Command clip) {
+        $password | clip
     }
-    Write-Host ""
-    Write-Host $password -ForegroundColor Yellow
-    Write-Host ""
-    Write-Host "This password has been copied to the clipboard." -ForegroundColor Green
+
+    return $password
 }
